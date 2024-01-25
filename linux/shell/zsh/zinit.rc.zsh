182058f40a69e21ab4f9ac9e27c11166f1cf49ed
takano536 <ohnishi.takanori@gmail.com>
2024-01-25 11:05:56 +0900

    zshにzと履歴のlistviewを導入

--- linux/shell/zsh/zinit.rc.zsh
+++ linux/shell/zsh/zinit.rc.zsh
@@ -1,10 +1,6 @@
-# auto suggestions
 repo='zsh-users/zsh-autosuggestions'
 zinit ice wait; zinit light $repo
 source "$XDG_DATA_HOME/zinit/plugins/${repo//\//---}/zsh-autosuggestions.zsh"
-
-# command completion
-zinit ice wait; zinit light zsh-users/zsh-completions
-
-# syntax highlighting
+zinit ice wait; zinit light marlonrichert/zsh-autocomplete
+zinit ice wait; zinit load agkozak/zsh-z
 zinit ice wait'!0'; zinit light zsh-users/zsh-syntax-highlighting
