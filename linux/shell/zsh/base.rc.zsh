76fa238f2acbcd51f01c48b787390d2c82ad1f56
takano536 <ohnishi.takanori@gmail.com>
2024-02-29 04:41:16 +0900

    zsh用のいい感じのキー入力補完を追加

--- linux/shell/zsh/base.rc.zsh
+++ linux/shell/zsh/base.rc.zsh
@@ -34,3 +34,11 @@ setopt share_history
 
 # ignore additional whitespaces
 setopt hist_reduce_blanks
+
+# いい感じのキー入力補完
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
+
