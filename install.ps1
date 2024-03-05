20d2d76c9e0ad387eb0b4f61f46d3de32cddf574
takano536 <ohnishi.takanori@gmail.com>
2024-03-06 08:08:01 +0900

    インストールスクリプトを修正

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@ $DOTFILES_DIR = "$env:USERPROFILE\.config"
 $TERMINAL_DIR = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 $POWERSHELL_DIR = "$env:USERPROFILE\Documents\PowerShell"
 $SUBLIME_DIR = "$env:LOCALAPPDATA\Scoop\persist\sublime-text\Data\Packages\User"
-$FIREFOX_DIR = "$env:LOCALAPPDATA\Scoop\persist\firefox\Profile\chrome"
+$FIREFOX_DIR = "$env:LOCALAPPDATA\Scoop\persist\firefox\Profile"
 
 function New-link {
     param(
@@ -21,9 +21,22 @@ function New-link {
     }
 }
 
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (!$isAdmin) {
+    Write-Output 'Error: Please run this script as an administrator'
+    exit 1
+}
+
+if (Test-Path $DOTFILES_DIR) {
+    Write-Output 'Error: The dotfiles directory already exists'
+    exit 1
+}
+
 git clone 'https://github.com/takano536/dotfiles.git' $DOTFILES_DIR
 New-link -ItemType SymbolicLink -Path "$TERMINAL_DIR\settings.json" -Target "$DOTFILES_DIR\windows\windows_terminal\settings.json"
 New-link -ItemType SymbolicLink -Path "$POWERSHELL_DIR\Microsoft.PowerShell_profile.ps1" -Target "$DOTFILES_DIR\windows\powershell\Microsoft.PowerShell_profile.ps1"
 New-link -ItemType SymbolicLink -Path "$SUBLIME_DIR\Preferences.sublime-settings" -Target "$DOTFILES_DIR\sublime\Preferences.sublime-settings"
-New-link -ItemType SymbolicLink -Path "$FIREFOX_DIR\userChrome.css" -Target "$DOTFILES_DIR\common\firefox\userChrome.css"
-New-link -ItemType Junction -Path "$FIREFOX_DIR\modules" -Target "$DOTFILES_DIR\common\firefox\modules"
+New-link -ItemType SymbolicLink -Path "$FIREFOX_DIR\chrome\userChrome.css" -Target "$DOTFILES_DIR\common\firefox\userChrome.css"
+New-link -ItemType Junction -Path "$FIREFOX_DIR\chrome\modules" -Target "$DOTFILES_DIR\common\firefox\modules"
+New-link -ItemType SymbolicLink -Path "$FIREFOX_DIR\user.js" -Target "$DOTFILES_DIR\common\firefox\user.js"
