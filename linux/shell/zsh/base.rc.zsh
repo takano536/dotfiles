74b5b9cc9893bd2b920345fa3fe3b53cb77d8ca5
takano536 <ohnishi.takanori@gmail.com>
2024-03-21 18:30:42 +0900

    zinit iceを消したらバグが消えたので修正

--- linux/shell/zsh/base.rc.zsh
+++ linux/shell/zsh/base.rc.zsh
@@ -34,18 +34,3 @@ setopt share_history
 
 # ignore additional whitespaces
 setopt hist_reduce_blanks
-
-# いい感じのキー入力補完
-# start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-    autoload -U up-line-or-beginning-search
-    zle -N up-line-or-beginning-search
-    bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
-# start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-    autoload -U down-line-or-beginning-search
-    zle -N down-line-or-beginning-search
-    bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
-
--- linux/shell/zsh/zinit.rc.zsh
+++ linux/shell/zsh/zinit.rc.zsh
@@ -1,6 +1,4 @@
-repo='zsh-users/zsh-autosuggestions'
-zinit ice wait; zinit light $repo
-source "$XDG_DATA_HOME/zinit/plugins/${repo//\//---}/zsh-autosuggestions.zsh"
-zinit ice wait; zinit light marlonrichert/zsh-autocomplete
-zinit ice wait; zinit load agkozak/zsh-z
-zinit ice wait'!0'; zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light marlonrichert/zsh-autocomplete
+zinit light agkozak/zsh-z
+zinit light zsh-users/zsh-syntax-highlighting
