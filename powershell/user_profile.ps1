0a84245bd071045e7e47ac0c00eb19972bc59ecc
takano536 <85817284+takano536@users.noreply.github.com>
2024-07-25 01:35:49 +0900

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -24,8 +24,8 @@ $importedModules = Get-Module
 if ((Get-Module -Name 'PSReadLine' -ListAvailable) -and !($importedModules.Name.Contains('PSReadLine'))) { Import-Module -Name PSReadLine }
 if ((Get-Module -Name 'CompletionPredictor' -ListAvailable) -and !($importedModules.Name.Contains('CompletionPredictor'))) { Import-Module -Name CompletionPredictor }
 if ((Get-Module -Name 'PowerType' -ListAvailable) -and !($importedModules.Name.Contains('PowerType'))) { Enable-PowerType }
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin 
-Set-PSReadLineOption -PredictionViewStyle ListView
+if ($PSVersionTable.PSVersion.Major -ge 7) { Set-PSReadLineOption -PredictionSource HistoryAndPlugin }
+if ($PSVersionTable.PSVersion.Major -ge 7) { Set-PSReadLineOption -PredictionViewStyle ListView }
 Set-PSReadlineOption -HistoryNoDuplicates
 Set-PSReadlineOption -EditMode Windows
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
