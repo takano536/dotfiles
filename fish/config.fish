65f94951ee5149fd7aa202a4eaadea241f054af8
takano536 <ohnishi.takanori@gmail.com>
2025-10-19 21:36:03 +0900

    Catppuccinテーマに変更

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,6 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # run runcoms
 source "$XDG_CONFIG_HOME/fish/env.rc.fish"
 source "$XDG_CONFIG_HOME/fish/base.rc.fish"
-source "$XDG_CONFIG_HOME/fish/theme.rc.fish"
 source "$XDG_CONFIG_HOME/fish/prompt.rc.fish"
 source "$XDG_CONFIG_HOME/fish/appinit.rc.fish"
 source "$XDG_CONFIG_HOME/fish/alias.rc.fish"
