ce8d4b719ba9d8a5050cdf70a22fe0c0fe16c83a
takano536 <85817284+takano536@users.noreply.github.com>
2026-09-02 11:06:11 +0000

    feat(fish): add fnm initialization

--- home/dot_config/fish/conf.d/20-path.fish
+++ home/dot_config/fish/conf.d/20-path.fish
@@ -1,3 +1,8 @@
 fish_add_path --global "$HOME/.local/bin"
 fish_add_path --global "$HOME/bin"
 fish_add_path --global "$HOME/.bun/bin"
+
+if test -x "$HOME/.local/share/fnm/fnm"
+    fish_add_path "$HOME/.local/share/fnm"
+    fnm env --use-on-cd --shell fish | source
+end
